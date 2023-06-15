418baebdd6c5c96f09b23ea69c5ec5ef7728fa2b
unknown <priyajeetsaxena@gmail.com>
2023-06-15 12:34:53 +0530

    made file smaller

--- src/components/Banner/banner.js
+++ src/components/Banner/banner.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import bannerImage from '../../assets/Banner/Banner.png';
+import bannerImage from '../../assets/Banner/Banner.jpg';
 
 const Banner = () => {
   const bannerStyle = {
