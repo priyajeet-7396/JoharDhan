f145aa6e0aa4c699c7654f812bdac2f568eaafe2
unknown <priyajeetsaxena@gmail.com>
2023-06-23 11:18:53 +0530

    made mobile responsive added form

--- src/components/Banner/banner.js
+++ src/components/Banner/banner.js
@@ -1,18 +1,40 @@
-import React from 'react';
-import bannerImage from '../../assets/Banner/Banner.jpg';
+// import React from 'react';
+// import './banner.css';
+// import bannerImage from '../../assets/Banner/Banner.jpg';
+// import bannerImageMobile from '../../assets/Banner/Banner_m.png';
+
+// const Banner = () => {
+//   return (
+//     <div className="banner"></div>
+//   );
+// };
+
+// export default Banner;
+
+
+import React, { useState, useEffect } from 'react';
+import desktopImage from '../../assets/Banner/Banner.jpg';
+import mobileImage from '../../assets/Banner/Banner_M.png';
+import './banner.css';
 
 const Banner = () => {
-  const bannerStyle = {
-    backgroundImage: `url(${bannerImage})`,
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-    height: '85vh', /* Adjust the height as needed */
-    marginTop: '120px', /* Add the margin top value */
-  
-  };
+  const [imageUrl, setImageUrl] = useState(window.innerWidth >= 768 ? desktopImage : mobileImage);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setImageUrl(window.innerWidth >= 768 ? desktopImage : mobileImage);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   return (
-    <div className="banner" style={bannerStyle}>
+    <div className="banner">
+      <img src={imageUrl} alt="Banner" />
     </div>
   );
 };
--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,18 +1,35 @@
-import { useRef } from "react"
+import { useRef } from "react";
+import emailjs from '@emailjs/browser';
+import './Contact.css'
 
 const Contact = () => {
-    const form = useRef()
-    const  sendEmail = () =>{};
+    const form = useRef();
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_2ck7pda', 'template_ezi0bfs', form.current, 'Qy-JEXbd8P7elG-kx')
+          .then((result) => {
+              console.log(result.text);
+          }, (error) => {
+              console.log(error.text);
+          });
+          e.target.reset()
+      };
+    
 
   return (
     <section>
-    <div className='container'>
+    <div className='container-form'>
         <h2 className='textcenter'>Contact Us</h2>
         <form ref={form} onSubmit={sendEmail} className='form-control-card'>
-            <input type='text' placeholder='Full Name' name='user_name' required />
-            <input type='email' placeholder='Email' name='user_email' required />
-            <input type='text' placeholder='Subject' name='subject' required />
-            <textarea name='message' cols="30" rows= "10"></textarea>
+            <input type='text' placeholder='Your Full Name' name='user_name' required />
+            <input type='text' placeholder='No. of delegates' name='user_number' required />
+            <input type='email' placeholder=' Your Email' name='user_email' required />
+            <input type="number" placeholder="Your Phone" name="user_phone" required />
+            <input type='text' placeholder='Your Organization' name='user_organization' required />
+            <input type='text' placeholder='Your Designation' name='user_designation' required />
+            <input type='text' placeholder='Your State' name='user_state' required />
+            <textarea name='user_address' placeholder='Address ' cols="10" rows= "10"></textarea>
             <button type='submit' className='btn'>Send Message</button>
         </form>
     </div>
@@ -21,3 +38,4 @@ const Contact = () => {
 }
 
 export default Contact
+
